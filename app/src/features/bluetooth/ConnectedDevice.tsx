84a13ba5158e64a8179e19158cf0c962f8d5e57d
Ryan Reszetnik <rres@rogers.com>
2023-11-28 14:26:39 -0500

    decoding messages

--- app/src/features/bluetooth/ConnectedDevice.tsx
+++ app/src/features/bluetooth/ConnectedDevice.tsx
@@ -1,5 +1,5 @@
 import { ScrollView, StyleSheet, Text, View } from "react-native";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { MyAppState } from "../../redux/store";
 import DevicePreview from "./components/DevicePreview";
@@ -10,7 +10,8 @@ import * as Bluetooth from "../../utils/ble";
 import AppTextInput from "../../components/AppTextInput";
 import { BLECharacteristic, BLEDevice } from "../../utils/ble/ble.types";
 import decodeMessage from "./utils/decodeMessage";
-// import Svg, { Circle, Rect } from 'react-native-svg';
+import moment from "moment"
+import Svg, { Circle, Rect } from 'react-native-svg';
 
 type Props = {};
 
@@ -22,7 +23,22 @@ const ConnectedDevice = (props: Props) => {
   const [characteristic, setCharacteristic] =
     React.useState<BLECharacteristic | null>(null);
   const [monitoring, setMonitoring] = useState(false);
+  const latestMessage= useMemo(() =>{
+    if(messages.length===0) return null
+    
+    const decoded= decodeMessage(messages[messages.length-1].message) 
+    return decoded[decoded.length-1]
+  }, [messages]); 
 
+  const graphData = useMemo(() => {
+    return messages.flatMap(message=>decodeMessage(message.message)).map(message=>{
+      return {
+        date: moment().add(message.timestamp, 'milliseconds').subtract(latestMessage?.timestamp,'milliseconds').toDate(),
+        value: message.noise
+      }
+     })
+
+  }, [messages, latestMessage]);
   useEffect(() => {
     if (device && device.state === "connected" && !monitoring) {
       setMonitoring(true);
@@ -53,29 +69,30 @@ const ConnectedDevice = (props: Props) => {
     >
       <AppButton title="Disconnect" onPress={disconnect} />
       <DevicePreview name={device.name} id={device.id} state={device.state} />
-      <Text>Send Message To Device:</Text>
-      <AppTextInput value={input} onChangeText={setInput} />
-      {/* <Svg height="50%" width="50%" viewBox="0 0 100 100" {...props}>
-      <Circle cx="50" cy="50" r="45" stroke="blue" strokeWidth="2.5" fill="green" />
-      <Rect x="15" y="15" width="70" height="70" stroke="red" strokeWidth="2" fill="yellow" />
-    </Svg> */}
-      <AppButton
+      {/* <Text>Send Message To Device:</Text>
+      <AppTextInput value={input} onChangeText={setInput} /> */}
+      <Svg height="50%" width="50%" viewBox="0 0 100 100">
+        <Circle cx="50" cy="50" r="45" stroke="blue" strokeWidth="2.5" fill="green" />
+        <Rect x="15" y="15" width="70" height="70" stroke="red" strokeWidth="2" fill="yellow" />
+      </Svg>
+      {/* <AppButton
         title="Send"
         disabled={!characteristic}
         onPress={() => {
           if (!characteristic) return;
           Bluetooth.sendMessageToCharacteristic(characteristic, input);
         }}
-      />
+      /> */}
+      <View>
+        <Text>Pressure: {latestMessage?.pressure}</Text>
+        <Text>Temperature: {latestMessage?.temperature}</Text>
+        <Text>Humidity: {latestMessage?.humidity}</Text>
+        <Text>Noise: {latestMessage?.noise}</Text>
+      </View>
+      {/* {['pressure','temperature','humidity','noise'].map((item) => (<View><Text>{decodeMessage(messages[messages.length-1].message)[item]}</Text></View>))} */}
       <Text>Messages Received From Device:</Text>
       <AppButton title="Clear" onPress={clearMessages} />
-
-      {messages.map((message, i) => (
-        <View key={i} style={{ flexDirection: "row" }}>
-          <Text>{message.message}</Text>
-          <Text>{JSON.stringify(decodeMessage(message.message))}</Text>
-        </View>
-      ))}
+     {/* <LineGraph points={graphData} color="#4484B2" animated/> */}
     </ScrollView>
   );
 };
--- app/src/features/bluetooth/utils/decodeMessage.ts
+++ app/src/features/bluetooth/utils/decodeMessage.ts
@@ -8,7 +8,29 @@ function convertStringToByteArray(str: string) {
   return bytes;
 }
 
+function combineBytes(byteArray: number[], start: number, numBytes: number) {
+  let result = 0;
+  for (let i = start; i < start + numBytes; i++) {
+    result = result << 8;
+    result += byteArray[i];
+  }
+  return result;
+}
+
 export default function decodeMessage(message: string) {
   const byteArray = convertStringToByteArray(base64.decode(message));
-  return byteArray;
+  const num_readings = combineBytes(byteArray, 0, 2);
+  const readings = [];
+  for (let i = 0; i < num_readings; i++) {
+    const start = 2 + i * 16;
+    const reading = {
+      timestamp: combineBytes(byteArray, start, 4),
+      pressure: combineBytes(byteArray, start + 4, 2),
+      temperature: combineBytes(byteArray, start + 6, 4) / 10,
+      humidity: combineBytes(byteArray, start + 10, 2),
+      noise: combineBytes(byteArray, start + 12, 4),
+    };
+    readings.push(reading);
+  }
+  return readings;
 }
